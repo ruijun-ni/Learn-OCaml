97f6143e63c2840bf7072feb22e0913936289329
Ruijun (Regina) Ni <75625953+reginanee@users.noreply.github.com>
2021-10-12 11:26:21 -0500

    Delete lab1 directory

--- lab1/howMany.ml
+++
@@ -1,9 +0,0 @@
- open List
- let rec howMany elem lst =
-    if lst = [] then 0
-    else
-        if elem = (hd lst) then
-            1 + howMany elem (tl lst)
-        else
-            howMany elem (tl lst)
-;;
